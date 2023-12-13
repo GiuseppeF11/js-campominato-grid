38f20f7db2cce0719f88f10494af74dd4b89c8bf
GiuseppeF11 <faillagiuseppe13@gmail.com>
2023-12-13 16:57:15 +0100

    Aggiunto Bonus con scelta differenti difficoltà

--- js/scripts.js
+++ js/scripts.js
@@ -10,14 +10,30 @@ const playBtn = document.querySelector('button')
 //Quando clicco il play button partirà il ciclo
 playBtn.addEventListener('click', function(){
 
+//BONUS
+const select = parseInt(document.getElementById('difficult').value);
+console.log(select +' '+ typeof select)
+
     //Il ciclo va da 1 a 100 (incluso) e si incrementa di un man mano
-    for (let i = 1; i <= 100; i++) {
+    for (let i = 1; i <= select; i++) {
 
         //Creo un elemento div e lo nomino cell
         const cell = document.createElement('div');
-
         //Assegno a cell la classe cell (vedi CSS) (così prende le sembianze di un quadrato)
         cell.classList.add('cell');
+
+        //Se seleziono 'Medium - 81' aggiungo la classe cell-81
+        if (select == '81') {
+            cell.classList.add('cell-81');
+        }
+
+        //Se seleziono 'Hard - 49' aggiungo la classe cell-49
+        else if (select == '49') {
+            cell.classList.add('cell-49');
+        }
+
+        //ALTRIMENTI se seleziono 'Easy - 100' aggiungo la classe cell-100
+        cell.classList.add('cell-100');
 
         //Metto l'indice (NB:che man mano si incrementa) nella cella, così da creare 100 celle quadrate
         cell.innerHTML = i;
